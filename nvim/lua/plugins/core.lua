6162a224e93e28049425c6da2932f0d5064a5ee6
Tim Earle <xortim@users.noreply.github.com>
2026-03-21 23:41:48 -0400

    remove submodule

---
+++ nvim/lua/plugins/core.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
